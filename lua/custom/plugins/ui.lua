8f438cbbaeb075f9725be766f521ee6f167b84bd
Samuel Vrielynck <samvrie@proton.me>
2024-04-05 11:35:02 -0400

    feat: setup neo-tree

---
+++ lua/custom/plugins/ui.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {},
+  keys = {
+    { 
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+      end,
+      desc = "Explorer NeoTree (Root Dir)",
+    },
+  },
+}
