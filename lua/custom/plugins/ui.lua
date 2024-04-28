59f842fb22762eaaf3bab2eef6fcbdcb3df9dc07
Samuel Vrielynck <samvrie@proton.me>
2024-04-28 21:38:27 +0300

    feat: add NeoTest

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -16,4 +16,29 @@ return {
       desc = "Explorer NeoTree (Root Dir)",
     },
   },
+  {
+  "nvim-neotest/neotest",
+  event = "VeryLazy",
+  dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-neotest/neotest-jest"
+    },
+  },
+  keys = {
+    {
+      "<leader>tr",
+      function()
+        require("neotest").run.run()
+      end,
+      desc = "Neo test run",
+    },
+  },
+  opts = function(_, opts) 
+    vim.list_extend(opts.adapters, {
+      require("nvim-neotest/neotest-jest")
+    })
+  end,
 }
