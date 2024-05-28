c0ce8d58ea442a66566eac574225ca8842840e58
Samuel Vrielynck <samvrie@proton.me>
2024-05-28 12:07:07 +0300

    chore: change bindings for neotest

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -92,13 +92,17 @@ return {
         "nvim-neotest/neotest-jest"
     },
     keys = {
-      {
-        "<leader>tr",
-        function()
-          require("neotest").run.run()
-        end,
-        desc = "Neo test run",
-      },
+      { "<leader>tu", function() require("neotest").summary.toggle() end, desc = "Toggle Summary", },
+      { "<leader>tr", function() require("neotest").run.run() end, desc = "Run Nearest" },
+      { "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
+      { "<leader>tT", function() require("neotest").run.run(vim.uv.cwd()) end, desc = "Run All Test Files" },
+      { "<leader>td", function() require("neotest").run.run({strategy = "dap"}) end, desc = "Debug Nearest" },
+      { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last" },
+      { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary" },
+      { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output" },
+      { "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel" },
+      { "<leader>tS", function() require("neotest").run.stop() end, desc = "Stop" },
+      { "<leader>tw", function() require("neotest").run.run({ jestCommand = 'jest --watch ' }) end, desc = "Run Tests in Watch mode" },
     },
     config = function()
       require('neotest').setup({
@@ -106,11 +110,15 @@ return {
           require('neotest-jest')({
             jestCommand = "npm test --",
             jestConfigFile = "jest.config.ts",
+            jest_test_discovery = true,
             env = { CI = true },
             cwd = function(path)
               return vim.fn.getcwd()
             end,
           }),
+        },
+        discovery = {
+          enabled = false,
         }
     })
     end,
