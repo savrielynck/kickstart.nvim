b8fd7724f7ab0d003b3f77586239dc3cabbd8f19
Samuel Vrielynck <samvrie@proton.me>
2024-05-28 12:07:37 +0300

    chore: small linting

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -1,10 +1,10 @@
 return {
-
   { -- Linting
     'mfussenegger/nvim-lint',
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
+
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
         javascript = { 'eslint_d' },
