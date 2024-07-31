a010d067af51ec7ee92a53236f9994cbd3c8624f
Samuel Vrielynck <samvrie@proton.me>
2024-07-31 15:16:00 +0300

    chore: update tree sitter list of installed languages

--- init.lua
+++ init.lua
@@ -850,7 +850,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'typescript', },
+      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'typescript', 'http', 'json', },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
